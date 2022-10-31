541a2612681bb63fd7d57d719f4c010ef21a1262
DANIEL <dvespac@gmail.com>
2022-11-01 00:53:20 +0100

    once

---
+++ app/src/main/java/com/novita/todorecetas/NavegationDrawer.java
@@ -0,0 +1,14 @@
+package com.novita.todorecetas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NavegationDrawer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navegation_drawer);
+    }
+}
---
+++ app/src/main/java/com/novita/todorecetas/RestablecerContrasena.java
@@ -0,0 +1,11 @@
+package com.novita.todorecetas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestablecerContrasena{
+
+
+
+}
