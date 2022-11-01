87305cd1a1fdc68299cae81f624eab3828b51ed5
DANIEL <dvespac@gmail.com>
2022-11-01 04:37:47 +0100

    Merge remote-tracking branch 'origin/cocina_kla' into cocina_kla

--- app/src/main/java/com/novita/todorecetas/NavegationDrawer.java
+++ app/src/main/java/com/novita/todorecetas/NavegationDrawer.java
@@ -1,14 +1,27 @@
 package com.novita.todorecetas;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class NavegationDrawer extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navegation_drawer);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
     }
 }
