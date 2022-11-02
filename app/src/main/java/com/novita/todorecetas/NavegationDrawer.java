16eb932de3eaee2fb6c49af78081e9d9949ba181
DANIEL <dvespac@gmail.com>
2022-11-02 01:24:20 +0100

    Merge remote-tracking branch 'origin/cocina_kla' into cocina_kla

--- app/src/main/java/com/novita/todorecetas/NavegationDrawer.java
+++ app/src/main/java/com/novita/todorecetas/NavegationDrawer.java
@@ -1,11 +1,13 @@
 package com.novita.todorecetas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
@@ -23,5 +25,20 @@ public class NavegationDrawer extends AppCompatActivity {
         setContentView(R.layout.activity_navegation_drawer);
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
     }
 }
